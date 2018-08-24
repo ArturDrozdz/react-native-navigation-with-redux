e9eedeeb97b64a841afef9cd3ece1b37502493dd
Artur Drozdz <artur.ixara@gmail.com>
2018-08-25 00:44:17 +0200

    wiring up redux

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 /** @format */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import React from 'react'
+import { AppRegistry } from 'react-native'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import reducers from './app/reducers'
+import Navigation from './app/components/navigation/navigation'
+import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// middleware that logs actions
+const loggerMiddleware = createLogger({ predicate: (getState, action) => __DEV__ });
+
+function configureStore(initialState) {
+	const enhancer = compose(
+		applyMiddleware(
+			thunkMiddleware, // lets us dispatch() functions
+			loggerMiddleware,
+		),
+	);
+	return createStore(reducers, initialState, enhancer);
+}
+
+const store = configureStore({});
+
+const Main = () => (
+	<Provider store={store}>
+		<Navigation />
+	</Provider>
+)
+
+AppRegistry.registerComponent(appName, () => Main);
